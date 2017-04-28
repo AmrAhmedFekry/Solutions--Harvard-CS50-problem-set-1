db683773149e507dbb787388c0dae648edac1101
Amr Ahmed Fekry <amrahmedfekry@outlook.com>
2017-04-28 18:02:03 +0200

    Create mario.c

---
+++ mario.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void){
+printf("Height: ");
+int height = get_int(); 
+for(int row = 0 ; row< height ; row++ ){
+for (int space = 0; space<height-row-1 ; space++ ) {
+    printf("%s" , " ");
+} 
+    for (int j = 0 ; j< row+2 ; j++){
+    printf("#");    
+    }
+    printf("\n");
+    
+}
+   
+
+
+}
+
+
+
